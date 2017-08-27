89dfc3a217a6e99f7508b135d668329fb73ce130
overriderb <overriderb@gmail.com>
2017-08-27 12:47:12 +0200

    Issue #1: Tests for all validation test cases

--- src/test/scala/battleship/BattleStartupTest.scala
+++ src/test/scala/battleship/BattleStartupTest.scala
@@ -12,67 +12,110 @@ class BattleStartupTest extends FlatSpec with Matchers {
   behavior of "BattleStartupTest"
 
   it should "successfully check that all ships in field of the map" in {
-    val fourDecker = Ship(List(Decker(Coordinate(1,1)), Decker(Coordinate(1,2)), Decker(Coordinate(1,3)), Decker(Coordinate(1,4))))
-    val threeDecker1 = Ship(List(Decker(Coordinate(3,1)), Decker(Coordinate(3,2)), Decker(Coordinate(3,3))))
-    val threeDecker2 = Ship(List(Decker(Coordinate(3,5)), Decker(Coordinate(3,6)), Decker(Coordinate(3,7))))
-    val twoDecker1 = Ship(List(Decker(Coordinate(5,1)), Decker(Coordinate(5,2))))
-    val twoDecker2 = Ship(List(Decker(Coordinate(5,4)), Decker(Coordinate(5,5))))
-    val twoDecker3 = Ship(List(Decker(Coordinate(5,7)), Decker(Coordinate(5,8))))
-    val oneDecker1 = Ship(List(Decker(Coordinate(7,1))))
-    val oneDecker2 = Ship(List(Decker(Coordinate(7,3))))
-    val oneDecker3 = Ship(List(Decker(Coordinate(7,5))))
-    val oneDecker4 = Ship(List(Decker(Coordinate(7,7))))
-    val ships = List(fourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, oneDecker4)
-    val map: BattleMap = BattleMap(ships)
+    val map: BattleMap = validMap
 
     BattleStartup.validateMap(map) should be (true)
   }
 
   it should "mark map as invalid because of one ship out of field of the map" in {
-    val fourDecker = Ship(List(Decker(Coordinate(11,1)), Decker(Coordinate(11,2)), Decker(Coordinate(11,3)), Decker(Coordinate(11,4))))
-    val threeDecker1 = Ship(List(Decker(Coordinate(3,1)), Decker(Coordinate(3,2)), Decker(Coordinate(3,3))))
-    val threeDecker2 = Ship(List(Decker(Coordinate(3,5)), Decker(Coordinate(3,6)), Decker(Coordinate(3,7))))
-    val twoDecker1 = Ship(List(Decker(Coordinate(5,1)), Decker(Coordinate(5,2))))
-    val twoDecker2 = Ship(List(Decker(Coordinate(5,4)), Decker(Coordinate(5,5))))
-    val twoDecker3 = Ship(List(Decker(Coordinate(5,7)), Decker(Coordinate(5,8))))
-    val oneDecker1 = Ship(List(Decker(Coordinate(7,1))))
-    val oneDecker2 = Ship(List(Decker(Coordinate(7,3))))
-    val oneDecker3 = Ship(List(Decker(Coordinate(7,5))))
-    val oneDecker4 = Ship(List(Decker(Coordinate(7,7))))
-    val ships = List(fourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, oneDecker4)
+    val invalidFourDecker = Ship(List(Decker(Coordinate(11,1)), Decker(Coordinate(11,2)), Decker(Coordinate(11,3)), Decker(Coordinate(11,4))))
+    val ships = List(invalidFourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, oneDecker4)
     val map: BattleMap = BattleMap(ships)
 
     BattleStartup.validateMap(map) should be (false)
   }
 
   it should "successfully check that map contains only 4 one-decker, 3 two-decker, 2 three-decker, 1 four-decker ships" in {
-    val fourDecker = Ship(List(Decker(Coordinate(1,1)), Decker(Coordinate(1,2)), Decker(Coordinate(1,3)), Decker(Coordinate(1,4))))
-    val threeDecker1 = Ship(List(Decker(Coordinate(3,1)), Decker(Coordinate(3,2)), Decker(Coordinate(3,3))))
-    val threeDecker2 = Ship(List(Decker(Coordinate(3,5)), Decker(Coordinate(3,6)), Decker(Coordinate(3,7))))
-    val twoDecker1 = Ship(List(Decker(Coordinate(5,1)), Decker(Coordinate(5,2))))
-    val twoDecker2 = Ship(List(Decker(Coordinate(5,4)), Decker(Coordinate(5,5))))
-    val twoDecker3 = Ship(List(Decker(Coordinate(5,7)), Decker(Coordinate(5,8))))
-    val oneDecker1 = Ship(List(Decker(Coordinate(7,1))))
-    val oneDecker2 = Ship(List(Decker(Coordinate(7,3))))
-    val oneDecker3 = Ship(List(Decker(Coordinate(7,5))))
-    val oneDecker4 = Ship(List(Decker(Coordinate(7,7))))
-    val ships = List(fourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, oneDecker4)
-    val map: BattleMap = BattleMap(ships)
+    val map: BattleMap = validMap
 
     BattleStartup.validateMap(map) should be (true)
   }
 
-  it should "try to validate an invalid map with only 1 one-decker ship" in {
+  it should "mark map as invalid map with only 1 one-decker ship" in {
     val oneDecker = Ship(List(Decker(Coordinate(1,1))))
     val map: BattleMap = BattleMap(List(oneDecker))
 
     BattleStartup.validateMap(map) should be (false)
   }
 
-  it should "try to validate an invalid map without ships" in {
+  it should "mark map as invalid map without ships" in {
     val map: BattleMap = BattleMap(List())
 
     BattleStartup.validateMap(map) should be (false)
   }
+
+  it should "mark map as invalid map with 2 four-decker ships" in {
+    val additionalFourDecker = Ship(List(Decker(Coordinate(1,7)), Decker(Coordinate(1,8)), Decker(Coordinate(1,9)), Decker(Coordinate(1,10))))
+    val ships = List(fourDecker, additionalFourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, oneDecker4)
+    val map: BattleMap = BattleMap(ships)
+
+    BattleStartup.validateMap(map) should be (false)
+  }
+
+  it should "successfully check that distance between all ships on map not less then one cell" in {
+    val map: BattleMap = validMap
+
+    BattleStartup.validateMap(map) should be (true)
+  }
+
+  it should "mark map as invalid when distance between two ships on map less then one cell" in {
+    val invalidOneDecker = Ship(List(Decker(Coordinate(7,6))))
+    val ships = List(fourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, invalidOneDecker)
+    val map: BattleMap = BattleMap(ships)
+
+    BattleStartup.validateMap(map) should be (false)
+  }
+
+  it should "successfully check that all ships on map have straight shape" in {
+    val map: BattleMap = validMap
+
+    BattleStartup.validateMap(map) should be (true)
+  }
+
+  it should "mark map as invalid when at least one ship has not straight shape" in {
+    val invalidFourDecker = Ship(List(Decker(Coordinate(8,1)), Decker(Coordinate(9,1)), Decker(Coordinate(10,1)), Decker(Coordinate(10,2))))
+    val ships = List(invalidFourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, oneDecker4)
+    val map: BattleMap = BattleMap(ships)
+
+    BattleStartup.validateMap(map) should be (false)
+  }
+
+  it should "successfully check that no overlapping ships on the map" in {
+    val map: BattleMap = validMap
+
+    BattleStartup.validateMap(map) should be (true)
+  }
+
+  it should "mark map as invalid when distance between two ships on map less then one cell" in {
+    val invalidOneDecker = Ship(List(Decker(Coordinate(7,5))))
+    val ships = List(fourDecker, threeDecker1, threeDecker2, twoDecker1, twoDecker2, twoDecker3, oneDecker1, oneDecker2, oneDecker3, invalidOneDecker)
+    val map: BattleMap = BattleMap(ships)
+
+    BattleStartup.validateMap(map) should be (false)
+  }
+
+  private val fourDecker = Ship(List(Decker(Coordinate(1,1)), Decker(Coordinate(1,2)), Decker(Coordinate(1,3)), Decker(Coordinate(1,4))))
+  private val threeDecker1 = Ship(List(Decker(Coordinate(3,1)), Decker(Coordinate(3,2)), Decker(Coordinate(3,3))))
+  private val threeDecker2 = Ship(List(Decker(Coordinate(3,5)), Decker(Coordinate(3,6)), Decker(Coordinate(3,7))))
+  private val twoDecker1 = Ship(List(Decker(Coordinate(5,1)), Decker(Coordinate(5,2))))
+  private val twoDecker2 = Ship(List(Decker(Coordinate(5,4)), Decker(Coordinate(5,5))))
+  private val twoDecker3 = Ship(List(Decker(Coordinate(5,7)), Decker(Coordinate(5,8))))
+  private val oneDecker1 = Ship(List(Decker(Coordinate(7,1))))
+  private val oneDecker2 = Ship(List(Decker(Coordinate(7,3))))
+  private val oneDecker3 = Ship(List(Decker(Coordinate(7,5))))
+  private val oneDecker4 = Ship(List(Decker(Coordinate(7,7))))
+
+  private val validMap: BattleMap = BattleMap(List(
+    fourDecker,
+    threeDecker1,
+    threeDecker2,
+    twoDecker1,
+    twoDecker2,
+    twoDecker3,
+    oneDecker1,
+    oneDecker2,
+    oneDecker3,
+    oneDecker4
+  ))
 
 }
